74f22c5c840fe8c77f559ec15586098e05390d5b
Kappa Gang Scripts <126305391+Elvanoau@users.noreply.github.com>
2024-03-08 06:42:44 +1000

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,1 +1,57 @@
+const resource = GetParentResourceName();
+let brethoOn = false;
 
+$(function() {
+    window.addEventListener('message', function(event) {
+        if(event.data.type == "open") {
+            if (brethoOn == false) {
+                $("#brethoimage").attr("src","images/bretho_off.png");
+                $("#bretho").css("display", "block");
+                $(".screeninfo").css("display", "none");
+            } else {
+                $("#brethoimage").attr("src","images/bretho.png");
+                $("#bretho").css("display", "block");
+                $(".screeninfo").css("display", "block");
+                $("#curvol").html(event.data.amount);
+            }
+        } else if(event.data.type == "close") {
+            $("#brethoimage").attr("src","images/bretho.png");
+            $("#bretho").css("display", "none");
+            $(".screeninfo").css("display", "none");
+        } else if(event.data.type == "data") {
+            $("#curvol").html(event.data.amount);
+        } else if (event.data.type == "over") {
+            $("#brethoimage").attr("src","images/bretho_over.png");
+            $("#curvol").html(event.data.amount);
+        }
+    });
+
+    document.onkeyup = function (data) {
+        if (data.key == "Escape") { // Escape key
+            $.post(`https://${resource}/escape`, JSON.stringify({}));
+        }
+    };
+
+    document.onkeyup = function (data) {
+        if (data.key == "E") { // E key
+            if (brethoOn == true) {
+                $.post(`https://${resource}/breth`, JSON.stringify({}));
+            }
+        }
+    };
+});
+
+function OnOff() {
+    if (brethoOn) {
+        brethoOn = false;
+        $("#brethoimage").attr("src","images/bretho_off.png");
+        $("#bretho").css("display", "block");
+        $(".screeninfo").css("display", "none");
+    } else {
+        brethoOn = true;
+        $("#brethoimage").attr("src","images/bretho.png");
+        $("#bretho").css("display", "block");
+        $(".screeninfo").css("display", "block");
+        $("#curvol").html("0.000");
+    };
+};
